5a4fafe38c6f8a23f99a44fcb50ed780e31ed366
Filip Malmberg <91657147+myrmark@users.noreply.github.com>
2022-11-08 09:22:47 +0100

    Update labelprinter.py
    Added check for drivers, and installation process if missing.
    Added check for printers, and installation process if missing.

--- labelprinter.py
+++ labelprinter.py
@@ -1,15 +1,16 @@
 #Author Filip Malmberg
 
 import keyring
+import lzma
 import os
 import pymysql
 import subprocess
+import tarfile
 
 from datetime import datetime
 from time import sleep
 
 dbpw = keyring.get_password("172.28.88.47", "simdbuploader")
-printer='ME340_lager'
 user = os.getlogin()
 
 
@@ -18,6 +19,76 @@ try:
 except Exception:
     print("labelfiles folder was not found. Creating folder")
     os.mkdir(f"/home/{user}/labelfiles")
+
+
+try:
+    ps = subprocess.Popen('lpinfo -m', stdout=subprocess.PIPE, shell=True)
+    drivers_check = subprocess.check_output(('grep', 'TTP-644MT'), stdin=ps.stdout)
+    ps.wait()
+    drivers_check = drivers_check.decode().strip()
+except Exception:
+    print(f"Printer drivers not installed. Installing..")
+    with lzma.open('drivers.tar.xz') as fd:
+        with tarfile.open(fileobj=fd) as tar:
+            content = tar.extractall('drivers')
+    os.chdir('drivers')
+    cmd = 'sudo ./install-driver'.split()
+    subprocess.run(cmd)
+
+
+try:
+    ps = subprocess.Popen('lpstat -p -d', stdout=subprocess.PIPE, shell=True)
+    printer_check = subprocess.check_output(('grep', 'TTP-644MT'), stdin=ps.stdout)
+    ps.wait()
+    printer_check = printer_check.decode().strip()
+except Exception:
+    print(f"Printer TTP-644MT not installed. Installing..")
+    cmd = 'lpadmin -p TTP-644MT -E -m tscbarcode/TTP-644MT.ppd -v lpd://172.28.88.43/queue -o PageSize=Custom.60x30mm'.split()
+    subprocess.run(cmd)
+
+
+try:
+    ps = subprocess.Popen('lpstat -p -d', stdout=subprocess.PIPE, shell=True)
+    printer_check = subprocess.check_output(('grep', 'ME340_lager'), stdin=ps.stdout)
+    ps.wait()
+    printer_check = printer_check.decode().strip()
+except Exception:
+    print(f"Printer ME340_lager not installed. Installing..")
+    cmd = 'lpadmin -p ME340_lager -E -m tscbarcode/ME340.ppd -v lpd://172.28.88.46/queue -o PageSize=Custom.60x30mm'.split()
+    subprocess.run(cmd)
+
+
+try:
+    ps = subprocess.Popen('lpstat -p -d', stdout=subprocess.PIPE, shell=True)
+    printer_check = subprocess.check_output(('grep', 'ME340_production'), stdin=ps.stdout)
+    ps.wait()
+    printer_check = printer_check.decode().strip()
+except Exception:
+    print(f"Printer ME340_production not installed. Installing..")
+    cmd = 'lpadmin -p ME340_production -E -m tscbarcode/ME340.ppd -v lpd://172.28.88.60/queue -o PageSize=Custom.60x30mm'.split()
+    subprocess.run(cmd)
+
+
+try:
+    ps = subprocess.Popen('lpstat -p -d', stdout=subprocess.PIPE, shell=True)
+    printer_check = subprocess.check_output(('grep', 'Zebra_ZT230_production'), stdin=ps.stdout)
+    ps.wait()
+    printer_check = printer_check.decode().strip()
+except Exception:
+    print(f"Printer Zebra_ZT230_production not installed. Installing..")
+    cmd = 'lpadmin -p Zebra_ZT230_production -E -v socket://172.28.88.44:9100 -o PageSize=Custom.101x152mm'.split()
+    subprocess.run(cmd)
+
+
+try:
+    ps = subprocess.Popen('lpstat -p -d', stdout=subprocess.PIPE, shell=True)
+    printer_check = subprocess.check_output(('grep', 'Zebra_ZT230_lager'), stdin=ps.stdout)
+    ps.wait()
+    printer_check = printer_check.decode().strip()
+except Exception:
+    print(f"Printer Zebra_ZT230_lager not installed. Installing..")
+    cmd = 'lpadmin -p Zebra_ZT230_lager -E -v socket://172.28.88.45:9100 -o PageSize=Custom.101x152mm'.split()
+    subprocess.run(cmd)
 
 
 def sqlquery(column,itemnumber):
@@ -30,6 +101,11 @@ def sqlquery(column,itemnumber):
         result = False
     return(result)
     db.close()
+
+
+title = 'Select printer: '
+options = ['TTP-644MT', 'ME340_production', 'Zebra_ZT230_production', 'ME340_lager', 'Zebra_ZT230_lager']
+printer, index = pick(options, title)
 
 
 def getitemnumber():
