67b142b3077722b2f1545cd457b0ec427895e245
Filip Malmberg <91657147+myrmark@users.noreply.github.com>
2022-11-08 11:04:03 +0100

    Update labelprinter.py
    Added sudo to lpadmin and lpstat

--- labelprinter.py
+++ labelprinter.py
@@ -23,7 +23,7 @@ except Exception:
 
 
 try:
-    ps = subprocess.Popen('lpinfo -m', stdout=subprocess.PIPE, shell=True)
+    ps = subprocess.Popen('sudo lpinfo -m', stdout=subprocess.PIPE, shell=True)
     drivers_check = subprocess.check_output(('grep', 'TTP-644MT'), stdin=ps.stdout)
     ps.wait()
     drivers_check = drivers_check.decode().strip()
@@ -38,57 +38,57 @@ except Exception:
 
 
 try:
-    ps = subprocess.Popen('lpstat -p -d', stdout=subprocess.PIPE, shell=True)
+    ps = subprocess.Popen('sudo lpstat -p -d', stdout=subprocess.PIPE, shell=True)
     printer_check = subprocess.check_output(('grep', 'TTP-644MT'), stdin=ps.stdout)
     ps.wait()
     printer_check = printer_check.decode().strip()
 except Exception:
     print(f"Printer TTP-644MT not installed. Installing..")
-    cmd = 'lpadmin -p TTP-644MT -E -m tscbarcode/TTP-644MT.ppd -v lpd://172.28.88.43/queue -o PageSize=Custom.60x30mm'.split()
+    cmd = 'sudo lpadmin -p TTP-644MT -E -m tscbarcode/TTP-644MT.ppd -v lpd://172.28.88.43/queue -o PageSize=Custom.60x30mm'.split()
     subprocess.run(cmd)
 
 
 try:
-    ps = subprocess.Popen('lpstat -p -d', stdout=subprocess.PIPE, shell=True)
+    ps = subprocess.Popen('sudo lpstat -p -d', stdout=subprocess.PIPE, shell=True)
     printer_check = subprocess.check_output(('grep', 'ME340_lager'), stdin=ps.stdout)
     ps.wait()
     printer_check = printer_check.decode().strip()
 except Exception:
     print(f"Printer ME340_lager not installed. Installing..")
-    cmd = 'lpadmin -p ME340_lager -E -m tscbarcode/ME340.ppd -v lpd://172.28.88.46/queue -o PageSize=Custom.60x30mm'.split()
+    cmd = 'sudo lpadmin -p ME340_lager -E -m tscbarcode/ME340.ppd -v lpd://172.28.88.46/queue -o PageSize=Custom.60x30mm'.split()
     subprocess.run(cmd)
 
 
 try:
-    ps = subprocess.Popen('lpstat -p -d', stdout=subprocess.PIPE, shell=True)
+    ps = subprocess.Popen('sudo lpstat -p -d', stdout=subprocess.PIPE, shell=True)
     printer_check = subprocess.check_output(('grep', 'ME340_production'), stdin=ps.stdout)
     ps.wait()
     printer_check = printer_check.decode().strip()
 except Exception:
     print(f"Printer ME340_production not installed. Installing..")
-    cmd = 'lpadmin -p ME340_production -E -m tscbarcode/ME340.ppd -v lpd://172.28.88.60/queue -o PageSize=Custom.60x30mm'.split()
+    cmd = 'sudo lpadmin -p ME340_production -E -m tscbarcode/ME340.ppd -v lpd://172.28.88.60/queue -o PageSize=Custom.60x30mm'.split()
     subprocess.run(cmd)
 
 
 try:
-    ps = subprocess.Popen('lpstat -p -d', stdout=subprocess.PIPE, shell=True)
+    ps = subprocess.Popen('sudo lpstat -p -d', stdout=subprocess.PIPE, shell=True)
     printer_check = subprocess.check_output(('grep', 'Zebra_ZT230_production'), stdin=ps.stdout)
     ps.wait()
     printer_check = printer_check.decode().strip()
 except Exception:
     print(f"Printer Zebra_ZT230_production not installed. Installing..")
-    cmd = 'lpadmin -p Zebra_ZT230_production -E -m drv:///sample.drv/zebra.ppd -v socket://172.28.88.44:9100 -o PageSize=Custom.101x152mm'.split()
+    cmd = 'sudo lpadmin -p Zebra_ZT230_production -E -m drv:///sample.drv/zebra.ppd -v socket://172.28.88.44:9100 -o PageSize=Custom.101x152mm'.split()
     subprocess.run(cmd)
 
 
 try:
-    ps = subprocess.Popen('lpstat -p -d', stdout=subprocess.PIPE, shell=True)
+    ps = subprocess.Popen('sudo lpstat -p -d', stdout=subprocess.PIPE, shell=True)
     printer_check = subprocess.check_output(('grep', 'Zebra_ZT230_lager'), stdin=ps.stdout)
     ps.wait()
     printer_check = printer_check.decode().strip()
 except Exception:
     print(f"Printer Zebra_ZT230_lager not installed. Installing..")
-    cmd = 'lpadmin -p Zebra_ZT230_lager -E -m drv:///sample.drv/zebra.ppd -v socket://172.28.88.45:9100 -o PageSize=Custom.101x152mm'.split()
+    cmd = 'sudo lpadmin -p Zebra_ZT230_lager -E -m drv:///sample.drv/zebra.ppd -v socket://172.28.88.45:9100 -o PageSize=Custom.101x152mm'.split()
     subprocess.run(cmd)
 
 
